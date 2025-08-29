1ae72999479f7c5234741759e185b26c9daafd6c
MadPenguin <badpenguindev@gmail.com>
2025-08-29 21:07:14 +0900

    Fix Indicator Position

--- Assets/Scripts/InGame/GameBoardController.cs
+++ Assets/Scripts/InGame/GameBoardController.cs
@@ -60,20 +60,33 @@ namespace ProjectABC.InGame
         private IEnumerator RotateIndicator(MatchState matchState)
         {
             var targetAngle = new Vector3(0f, 0f, -90f);
+            var targetPoint = new Vector3(-1f, -1.5f, 0.5f);
             if (matchState == MatchState.Attacking)
+            {
                 targetAngle.z = 90f;
+                targetPoint.x = 1f;
+            }
             
             var localEulerAngles = _defenceIndicator.localEulerAngles;
             var startEuler = localEulerAngles;
             var endEuler = targetAngle;
+            
+            var startPosition = _defenceIndicator.localPosition;
+            var endPosition = targetPoint;
+            
             for (var t = 0f; t <= 1f; t += Time.deltaTime / 0.25f)
             {
                 var targetEuler = Vector3.Lerp(startEuler, endEuler, t);
                 _defenceIndicator.transform.localEulerAngles = targetEuler;
+                
+                var targetPosition = Vector3.Lerp(startPosition, endPosition, t);
+                _defenceIndicator.transform.localPosition = targetPosition;
                 yield return null;
             }
 
-            _defenceIndicator.transform.localEulerAngles = endEuler;
+            var indicatorPosition = _defenceIndicator.transform;
+            indicatorPosition.localEulerAngles = endEuler;
+            indicatorPosition.localPosition = endPosition;
         }
 
         public void OnEndRound()
